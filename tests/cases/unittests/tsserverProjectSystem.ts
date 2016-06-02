19c9a5319d46fb0f18b09cb9e718ff9a802ee110
Vladimir Matveev <vladima@microsoft.com>
2016-06-02 00:01:42 -0700

    added more tests

--- tests/cases/unittests/tsserverProjectSystem.ts
+++ tests/cases/unittests/tsserverProjectSystem.ts
@@ -7,7 +7,7 @@ namespace ts {
 
     const nullLogger: server.Logger = {
         close: () => void 0,
-        isVerbose: () => void 0, 
+        isVerbose: () => void 0,
         loggingEnabled: () => false,
         perftrc: () => void 0,
         info: () => void 0,
@@ -17,6 +17,10 @@ namespace ts {
     };
 
     const { content: libFileContent } = Harness.getDefaultLibraryFile(Harness.IO);
+
+    function getExecutingFilePathFromLibFile(libFile: FileOrFolder): string {
+        return combinePaths(getDirectoryPath(libFile.path), "tsc.js");
+    }
 
     interface FileOrFolder {
         path: string;
@@ -62,17 +66,40 @@ namespace ts {
         return entry;
     }
 
+    function sizeOfMap(map: Map<any>): number {
+        let n = 0;
+        for (const name in map) {
+            if (hasProperty(map, name)) {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    function checkMapKeys(caption: string, map: Map<any>, expectedKeys: string[]) {
+        assert.equal(sizeOfMap(map), expectedKeys.length, `${caption}: incorrect size of map`);
+        for (const name of expectedKeys) {
+            assert.isTrue(hasProperty(map, name), `${caption} is expected to contain ${name}, actual keys: ${getKeys(map)}`);
+        }
+    }
+
+    function checkFileNames(caption: string, actualFileNames: string[], expectedFileNames: string[]) {
+        assert.equal(actualFileNames.length, expectedFileNames.length, `${caption}: incorrect actual number of files, expected ${JSON.stringify(expectedFileNames)}, got ${actualFileNames}`);
+        for (const f of expectedFileNames) {
+            assert.isTrue(contains(actualFileNames, f), `${caption}: expected to find ${f} in ${JSON.stringify(actualFileNames)}`);
+        }
+    }
 
     function readDirectory(folder: FSEntry, ext: string, excludes: Path[], result: string[]): void {
-        if (!folder || !isFolder(folder) || excludes.indexOf(folder.path) >= 0) {
+        if (!folder || !isFolder(folder) || contains(excludes, folder.path)) {
             return;
         }
         for (const entry of folder.entries) {
-            if (excludes.indexOf(entry.path) >= 0) {
+            if (contains(excludes, entry.path)) {
                 continue;
             }
             if (isFolder(entry)) {
-                readDirectory(folder, ext, excludes, result);
+                readDirectory(entry, ext, excludes, result);
             }
             else if (fileExtensionIs(entry.path, ext)) {
                 result.push(entry.fullPath);
@@ -81,20 +108,27 @@ namespace ts {
     }
 
     class TestServerHost implements server.ServerHost {
-        args:string[] = [];
+        args: string[] = [];
         newLine: "\n";
 
         private fs: ts.FileMap<FSEntry>;
+        private getCanonicalFileName: (s: string) => string;
         private toPath: (f: string) => Path;
+        readonly watchedDirectories: Map<{ cb: DirectoryWatcherCallback, recursive: boolean }[]> = {};
+        readonly watchedFiles: Map<FileWatcherCallback[]> = {};
 
-        constructor(public useCaseSensitiveFileNames: boolean, private executingFilePath: string, private currentDirectory: string, ...fileOrFolderList: FileOrFolder[]) {
-            const getCanonicalName = createGetCanonicalFileName(useCaseSensitiveFileNames);
-            this.toPath = s => ts.toPath(s, currentDirectory, getCanonicalName);
+        constructor(public useCaseSensitiveFileNames: boolean, private executingFilePath: string, private currentDirectory: string, fileOrFolderList: FileOrFolder[]) {
+            this.getCanonicalFileName = createGetCanonicalFileName(useCaseSensitiveFileNames);
+            this.toPath = s => toPath(s, currentDirectory, this.getCanonicalFileName);
 
+            this.reloadFS(fileOrFolderList);
+        }
+
+        reloadFS(filesOrFolders: FileOrFolder[]) {
             this.fs = createFileMap<FSEntry>();
-            for (const fileOrFolder of fileOrFolderList) {
+            for (const fileOrFolder of filesOrFolders) {
                 const path = this.toPath(fileOrFolder.path);
-                const fullPath = getNormalizedAbsolutePath(fileOrFolder.path, currentDirectory);
+                const fullPath = getNormalizedAbsolutePath(fileOrFolder.path, this.currentDirectory);
                 if (typeof fileOrFolder.content === "string") {
                     const entry = { path, content: fileOrFolder.content, fullPath };
                     this.fs.set(path, entry);
@@ -105,12 +139,13 @@ namespace ts {
                 }
             }
         }
+
         fileExists(s: string) {
             const path = this.toPath(s);
             return this.fs.contains(path) && isFile(this.fs.get(path));
         };
 
-        directoryExists(s: string)  {
+        directoryExists(s: string) {
             const path = this.toPath(s);
             return this.fs.contains(path) && isFolder(this.fs.get(path));
         }
@@ -122,16 +157,53 @@ namespace ts {
             }
             else {
                 const entry = this.fs.get(path);
-                return isFolder(entry) ? map(entry.entries, x => getBaseFileName(x.fullPath)) : []
+                return isFolder(entry) ? map(entry.entries, x => getBaseFileName(x.fullPath)) : [];
             }
         }
 
         readDirectory(path: string, ext: string, excludes: string[]): string[] {
             const result: string[] = [];
-            readDirectory(this.fs.get(this.toPath(path)), ext, map(excludes, this.toPath), result);
+            readDirectory(this.fs.get(this.toPath(path)), ext, map(excludes, e => toPath(e, path, this.getCanonicalFileName)), result);
             return result;
-        } 
+        }
 
+        watchDirectory(directoryName: string, callback: DirectoryWatcherCallback, recursive: boolean): DirectoryWatcher {
+            const path = this.toPath(directoryName);
+            const callbacks = lookUp(this.watchedDirectories, path) || (this.watchedDirectories[path] = []);
+            callbacks.push({ cb: callback, recursive });
+            return {
+                referenceCount: 0,
+                directoryName,
+                close: () => {
+                    for (let i = 0; i < callbacks.length; i++) {
+                        if (callbacks[i].cb === callback) {
+                            callbacks.splice(i, 1);
+                            break;
+                        }
+                    }
+                    if (!callbacks.length) {
+                        delete this.watchedDirectories[path];
+                    }
+                }
+            };
+        }
+
+        watchFile(fileName: string, callback: FileWatcherCallback) {
+            const path = this.toPath(fileName);
+            const callbacks = lookUp(this.watchedFiles, path) || (this.watchedFiles[path] = []);
+            callbacks.push(callback);
+            return {
+                close: () => {
+                    const i = callbacks.indexOf(callback);
+                    callbacks.splice(i, 1);
+                    if (!callbacks.length) {
+                        delete this.watchedFiles[path];
+                    }
+                }
+            };
+        }
+
+        // TOOD: record and invoke callbacks to simulate timer events
         readonly setTimeout = (callback: (...args: any[]) => void, ms: number, ...args: any[]): any => void 0;
         readonly clearTimeout = (timeoutId: any): void => void 0;
         readonly readFile = (s: string) => (<File>this.fs.get(this.toPath(s))).content;
@@ -142,10 +214,9 @@ namespace ts {
         readonly write = (s: string) => notImplemented();
         readonly createDirectory = (s: string) => notImplemented();
         readonly exit = () => notImplemented();
-        readonly watchDirectory = (path: string, callback: DirectoryWatcherCallback, recursive: boolean): FileWatcher => void 0;
-        readonly watchFile = (path: string, callback: FileWatcherCallback): FileWatcher => void 0;
     }
-    describe("tsserver project system", () => {
+
+    describe("tsserver project system:", () => {
         it("create inferred project", () => {
             const appFile: FileOrFolder = {
                 path: "/a/b/c/app.ts",
@@ -155,19 +226,69 @@ namespace ts {
                 `
             };
             const libFile: FileOrFolder = {
-                path:  "/a/lib/lib.d.ts",
+                path: "/a/lib/lib.d.ts",
                 content: libFileContent
             };
             const moduleFile: FileOrFolder = {
                 path: "/a/b/c/module.d.ts",
                 content: `export let x: number`
             };
-            const host = new TestServerHost(/*useCaseSensitiveFileNames*/ false, getDirectoryPath(libFile.path), "/", appFile, moduleFile, libFile);
+            const host = new TestServerHost(/*useCaseSensitiveFileNames*/ false, getExecutingFilePathFromLibFile(libFile), "/", [appFile, moduleFile, libFile]);
             const projectService = new server.ProjectService(host, nullLogger);
-            const { configFileName, configFileErrors } = projectService.openClientFile(appFile.path);
+            const { configFileName } = projectService.openClientFile(appFile.path);
 
-            assert(!configFileName, "should not find config, got " + configFileName);
+            assert(!configFileName, `should not find config, got: '${configFileName}`);
             assert.equal(projectService.inferredProjects.length, 1, "expected one inferred project");
-        })
-    })
+            assert.equal(projectService.configuredProjects.length, 0, "expected no configured project");
+
+            const project = projectService.inferredProjects[0];
+
+            checkFileNames("inferred project", project.getFileNames(), [appFile.path, libFile.path, moduleFile.path]);
+            checkMapKeys("watchedDirectories", host.watchedDirectories, ["/a/b/c", "/a/b", "/a"]);
+        });
+
+        it("create configured project without file list", () => {
+            const configFile: FileOrFolder = {
+                path: "/a/b/tsconfig.json",
+                content: `
+                {
+                    "compilerOptions": {},
+                    "exclude": [
+                        "e"
+                    ]
+                }`
+            };
+            const libFile: FileOrFolder = {
+                path: "/a/lib/lib.d.ts",
+                content: libFileContent
+            };
+            const file1: FileOrFolder = {
+                path: "/a/b/c/f1.ts",
+                content: "let x = 1"
+            };
+            const file2: FileOrFolder = {
+                path: "/a/b/d/f2.ts",
+                content: "let y = 1"
+            };
+            const file3: FileOrFolder = {
+                path: "/a/b/e/f3.ts",
+                content: "let z = 1"
+            };
+            const host = new TestServerHost(/*useCaseSensitiveFileNames*/ false, getExecutingFilePathFromLibFile(libFile), "/", [ configFile, libFile, file1, file2, file3 ]);
+            const projectService = new server.ProjectService(host, nullLogger);
+            const { configFileName, configFileErrors } = projectService.openClientFile(file1.path);
+
+            assert(configFileName, "should find config file");
+            assert.isTrue(!configFileErrors, `expect no errors in config file, got ${JSON.stringify(configFileErrors)}`);
+            assert.equal(projectService.inferredProjects.length, 0, "expected no inferred project");
+            assert.equal(projectService.configuredProjects.length, 1, "expected one configured project");
+
+            const project = projectService.configuredProjects[0];
+            checkFileNames("configuredProjects project, actualFileNames", project.getFileNames(), [file1.path, libFile.path, file2.path]);
+            checkFileNames("configuredProjects project, rootFileNames", project.getRootFiles(), [file1.path, file2.path]);
+
+            checkMapKeys("watchedFiles", host.watchedFiles, [configFile.path, file2.path, libFile.path]); // watching all files except one that was open
+            checkMapKeys("watchedDirectories", host.watchedDirectories, [getDirectoryPath(configFile.path)]);
+        });
+    });
 }
